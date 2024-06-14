72bd6eb1c0f28c2d4068de93560ffff04c026e9f
Denis Zhuchkov <denis.bugir@gmail.com>
2024-06-14 13:47:17 +0300

    Second  task

---
+++ Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+void Main()
+{
+    int firstNumber = ReadInt("Задайте первое число: ");
+    int secondNumber = ReadInt("Задайте второе число: ");
+    Console.WriteLine(AckermannFunction(firstNumber, secondNumber));
+}
+
+int AckermannFunction(int number1, int number2)
+{
+    if (number1 == 0)
+    {
+        return number2 + 1;
+    }
+    else if (number2 == 0)
+    {
+        return AckermannFunction(number1 - 1, 1);
+    }
+    else
+    {
+        return AckermannFunction(number1 - 1, AckermannFunction(number1, number2 - 1));
+    }
+}
+
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+
+Main();
